58761854ca92ddec9dd70173302c17949033d57e
snusav1j <snusav1j>
2025-04-19 03:35:16 +0300

    update users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,33 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
+  devise_for :users, path: "", skip: [:unlocks], path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     sign_up: 'signup'
   }
   devise_scope :user do
-    get    'signup',  to: 'devise/registrations#new', as: :signup
-    get    'login',   to: 'devise/sessions#new',       as: :login
-    delete 'logout',  to: 'devise/sessions#destroy',   as: :logout
+    # Аутентификация
+    get    'signup',  to: 'devise/registrations#new',     as: :signup
+    get    'login',   to: 'devise/sessions#new',           as: :login
+    delete 'logout',  to: 'devise/sessions#destroy',       as: :logout
+  
+    # Редактирование профиля
+    get    'settings', to: 'devise/registrations#edit',   as: :edit_user
+  
+    # Восстановление пароля
+    get    'forgot',   to: 'devise/passwords#new',         as: :forgot_password
+    post   'forgot',   to: 'devise/passwords#create'
+    get    'reset',    to: 'devise/passwords#edit',        as: :edit_password
+    put    'reset',    to: 'devise/passwords#update'
+  
+    # Подтверждение почты
+    get    'confirm',  to: 'devise/confirmations#new',     as: :new_confirmation
+    post   'confirm',  to: 'devise/confirmations#create'
+    get    'confirm/verify', to: 'devise/confirmations#show', as: :confirm_verify
+  
+    # Разблокировка аккаунта
+    get    'unlock',   to: 'devise/unlocks#new',           as: :new_unlock
+    post   'unlock',   to: 'devise/unlocks#create'
+    get    'unlock/verify', to: 'devise/unlocks#show',     as: :unlock_verify
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -15,10 +35,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
 
   root "home#index"
-  # get "/signup", to: "users#new"
-  # get "/login", to: "sessions#new"
-  # get "/user_profiles/", to: "user_profiles#index"
-  # get "/logout", to: "sessions#logout"
+
 
   resources :home do
     collection do
